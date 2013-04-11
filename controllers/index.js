20e3f24dc576a82870dbb4896236028e03636f13
Maido Käära <maidokaara@gmail.com>
2013-04-11 12:43:25 +0100

    Makseviisi valideerimine

--- controllers/index.js
+++ controllers/index.js
@@ -5,26 +5,64 @@ var paytypes = require('../models/paytypes');
 var async = require('async');
 //Kassa
 exports.index = function(req, res){
-    async.parallel([
-            async.apply(transactions.getWithFilter,{hidden: false, invalid: false}),
-            async.apply(products.getAll),
-            async.apply(paytypes.getAll)
-        ]
+    async.parallel({
+            transactions: async.apply(transactions.getWithFilter,{hidden: false, invalid: false}),
+            products: async.apply(products.getAll),
+            paytypes: async.apply(paytypes.getAll)
+        }
         ,function(err, result){
-            res.render('index', { title: 'Kassa', transactions: result[0], products: result[1], paytypes: result[2]});
+            res.render('index', { title: 'Kassa', transactions: result.transactions, products: result.products, paytypes: result.paytypes});
         });
 };
 
 exports.transaction = function(req, res){
+    async.series({
+        allowed: async.apply(isPaymentTypeAllowed, req),
+        created: async.apply(createTransaction, req)
+    },
+    function(err, result){
+        if(err){
+            res.send({status: err});
+        }
+        else if(!result.allowed) {
+            res.send({status: "invalid_paytype"});
+        }
+        else if(!result.created){
+            res.send({status: "creation_failed"});
+        }
+        else{
+            res.send({status: "success"});
+        }
+    });
+}
+
+var isPaymentTypeAllowed = function(req, callback){
+    async.parallel({
+        user: async.apply(users.get, req.body.user),
+        paytypes: async.apply(paytypes.getAll)
+    },
+    function(err, result){
+        if(err){
+            callback(err, false);
+        }
+        else {
+            for(i=0;i<result.paytypes.length;i++){
+                var paytype = result.paytypes[i];
+                if(paytype.name == req.body.type){
+                    callback(null, paytype.allowedForStatus.indexOf(result.user.status) != -1);
+                    break;
+                }
+            }
+        }
+    });
+}
+
+var createTransaction = function(req, callback){
     var parseQuantity = function(reqProd, callback){
         reqProd.quantity = Number(reqProd.quantity);
         callback();
     }
     async.each(req.body.products, parseQuantity, function(err){
-        if(err != null){
-            res.send({status: err});
-            return;
-        }
         var transaction = {
             time: new Date(),
             user: req.body.user,
@@ -35,7 +73,7 @@ exports.transaction = function(req, res){
         }
         transactions.save(transaction, function(err, sum, id){
             var end = function(err){
-                res.send({status: err == null ? 'success' : err, sum: sum, id: id});
+                callback(err, err == null);
             }
             err == null ? decrementProductsAndBalance(transaction, end) : end(err);
         });
