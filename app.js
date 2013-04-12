0c7af11e0120f28e19616b4e26606feb4b797f23
Maido Käära <maidokaara@gmail.com>
2013-04-13 00:41:49 +0100

    Passport

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-
 //For config files
 var nconf = require('./nconf');
+
+//For authentication
+var passport = require('passport'),
+    DigestStrategy = require('passport-http').DigestStrategy;
+
+passport.use(new DigestStrategy({ qop: 'auth' },
+  function(username, done) {
+      if (username != nconf.get("admin:username")) { return done(null, false); }
+      return done(null, username, nconf.get("admin:password"));
+  }
+));
 
 //Web framework
 var express = require('express'),
@@ -18,6 +28,7 @@ app.configure(function(){
   app.use(express.compress());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(passport.initialize());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
--- routes.js
+++ routes.js
@@ -7,22 +7,25 @@ var statuses = require('./controllers/statuses');
 var paytypes = require('./controllers/paytypes');
 var stocktakings = require('./controllers/stocktakings');
 
+var passport = require('passport');
+
 module.exports = function(app) {
-    //Main views
-    app.get('/', index.index);
-    app.get('/admin', admin.index);
+    
     //Partials
     app.get('/admin/:page', admin.page);
     app.get('/dialog/:dialog', admin.dialog);
 
-    //Ajax requests
-
-    //Pos actions
+    //Pos view
+    app.get('/', index.index);
+    //Pos authenticated actions
     app.get('/posdata', index.posdata);
     app.post('/transaction', index.transaction);
     app.post('/transaction/invalid', index.invalid);
 
-    //Admin actions
+    //Admin view
+    app.get('/admin', authAdmin, admin.index);
+
+    //Admin authenticated actions
 
     //Users
     app.get('/users', users.all);
@@ -52,4 +55,8 @@ module.exports = function(app) {
     app.post('/stocktakings/generate', stocktakings.generate);
     app.get('/stocktakings/csv/:id', stocktakings.csv);
     app.get('/stocktakings/html/:id', stocktakings.html);
-}
+}}
+
+var authAdmin = passport.authenticate('digest', {session: false});
+
+var authPos = function(){}
